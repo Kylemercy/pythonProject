7db36d1c9a952c788a51ce59eda3473ff9499793
Mercy Obed <obedmercy2018@gmail.com>
2022-07-19 03:50:34 -0700

    learning python

---
+++ learn python/varaibles.py
@@ -0,0 +1,5 @@
+ #name genertor
+print('Welcome to the Band Name Generator.')
+name = input("what's name of the city you grew up in? \n")
+pet = input("what's name of your pet? \n")
+print('The name of your band is ' + name + ' '+ pet+'.')
