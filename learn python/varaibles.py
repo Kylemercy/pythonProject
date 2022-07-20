ad928d2361beaaa11af8e096f6de686893453154
Mercy Obed <obedmercy2018@gmail.com>
2022-07-20 04:33:21 -0700

    python codes on list operations

--- learn python/varaibles.py
+++ learn python/varaibles.py
@@ -3,3 +3,13 @@ print('Welcome to the Band Name Generator.')
 name = input("what's name of the city you grew up in? \n")
 pet = input("what's name of your pet? \n")
 print('The name of your band is ' + name + ' '+ pet+'.')
+#data types
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+a = two_digit_number[0]
+b = two_digit_number[1]
+print(int(a)+int(b))
