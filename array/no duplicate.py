579d79c1aa9c70d4cc27e61d089b16dce84fce06
Mercy Obed <obedmercy2018@gmail.com>
2022-07-23 15:07:44 +0100

    checking if a list or array dosent contain duplicate numbers

---
+++ array/no duplicate.py
@@ -0,0 +1,12 @@
+my_list = [2,3,4,2,5,6,7,8,9,11,13,14,15]
+
+def unique(my_list):
+    empty_list = []
+    for i in (my_list):
+        if i in empty_list:
+            print(i)
+            return False
+        else:
+            empty_list.append(i)
+    return True
+print(unique(my_list))
