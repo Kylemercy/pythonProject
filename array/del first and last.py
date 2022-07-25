c07ce8312dd4f0b2740ad34831359032e1de5c29
Mercy Obed <obedmercy2018@gmail.com>
2022-07-25 13:38:21 +0100

    deleting first and last element from a list

--- array/del first and last.py
+++ array/del first and last.py
@@ -2,8 +2,6 @@ def middle(mylist):
     newlist = mylist[1: ]
     del newlist[-1]
     return newlist
-    newlist = [1: ]
-    del newlist[-1]
-    return newlist
+
 mylist = [1,2,3,4]
 print(middle(mylist))
