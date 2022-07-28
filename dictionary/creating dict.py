7d3f9fef4aa54af16a92807b83f470f32da55c17
Mercy Obed <obedmercy2018@gmail.com>
2022-07-28 01:34:43 +0100

    creating a dictionary

---
+++ dictionary/creating dict.py
@@ -0,0 +1,6 @@
+#cereatig a dict
+sports = {'peter': 'football','john':'golf','ken':'running','dan':'tennis','dil':'swimming'}
+print(sports)
+print(sports['peter'])
+sports['peter'] = 'basket ball'
+print(sports)
