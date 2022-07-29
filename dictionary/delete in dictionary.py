8c392cc413b12ceacfbd3df0cd2fc7e752f87dfb
Mercy Obed <obedmercy2018@gmail.com>
2022-07-29 10:50:01 +0100

    deleting items ,key,value in a dictionary

---
+++ dictionary/delete in dictionary.py
@@ -0,0 +1,13 @@
+#deleting in a dict
+#deletion using the pop fun that use abkey
+sports = {'peter': 'football', 'john': 'golf', 'ken': 'running', 'dan': 'tennis', 'dil': 'swimming'}
+print(sports)
+print(sports.pop('peter'))
+print(sports)
+#using popitem
+print(sports.popitem())
+#delets any random item in the dict
+print(sports)
+# using clear or del
+del sports['ken']
+print(sports)
