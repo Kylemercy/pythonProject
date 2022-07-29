13394734321be977713be469a1a3ab923c593f48
Mercy Obed <obedmercy2018@gmail.com>
2022-07-29 10:40:19 +0100

    travasing through a dictionary

---
+++ dictionary/travasing a dict.py
@@ -0,0 +1,7 @@
+sports = {'peter': 'football','john':'golf','ken':'running','dan':'tennis','dil':'swimming'}
+print(sports)
+def travasing_dict(sports):
+
+    for key in sports:
+        print(key,sports[key])
+travasing_dict(sports)
