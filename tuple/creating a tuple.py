431d4c0bcd6d18be986002c4e386ec8a6e582e56
MERCY <obedmercy2018@gmail.com>
2022-08-03 16:48:31 +0100

    creating a tuple file

---
+++ tuple/creating a tuple.py
@@ -0,0 +1,9 @@
+# creating a tuple
+newtuple = ('a','b','g','t')
+print(newtuple)
+# for single element tuple always add a comma at the end to identify it as a tuple
+tuple1 = ('a',)
+print(tuple1)
+#creating tuple function to create a tuple
+newtup = tuple('asvusdtdnisynags')
+print(newtup)
