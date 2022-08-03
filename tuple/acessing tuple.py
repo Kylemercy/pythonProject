34cbf527058570aad2a25933522a42353ab3d6c6
MERCY <obedmercy2018@gmail.com>
2022-08-03 17:23:45 +0100

    accessing a tuple file

---
+++ tuple/acessing tuple.py
@@ -0,0 +1,8 @@
+#acessing element in tuple
+newtuple = ('a','b','g','t','r','h','k')
+print(newtuple[1])
+print(newtuple[-1])
+# accessing using slice operator
+print(newtuple[1:3])
+print(newtuple[:3])
+# note we cant update a tuple as it is immutable
