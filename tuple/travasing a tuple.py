ef89b6253d850b53db519087be31f33e92434af6
MERCY <obedmercy2018@gmail.com>
2022-08-03 17:31:52 +0100

    travasing through a tuple file

---
+++ tuple/travasing a tuple.py
@@ -0,0 +1,7 @@
+# traversing a tuple
+newtuple = ('a', 'b', 'g', 't', 'r', 'h', 'k')
+for i in newtuple:
+    print(i)
+# traversing using range function
+for i in range(len(newtuple)):
+    print(i,newtuple[i])
