0aa0270ebc8989d444f01deaad7e7bc4dcc40e75
MERCY <obedmercy2018@gmail.com>
2022-08-03 18:11:34 +0100

    operations and methods of tuple

---
+++ tuple/tuple operators and fun.py
@@ -0,0 +1,20 @@
+# adding 2 of tuples
+newtuple = ('a', 'b', 'g', 't', 'r', 'b', 'h', 'k')
+mytuple = ('d', 'a', 'u', 'n', 'r')
+print(newtuple + mytuple)
+print(newtuple * 3)
+# in operator
+print(' j' in newtuple)
+# method in tuple
+# count method
+print(newtuple.count('r'))
+print(newtuple.count('b'))
+# index method
+print(newtuple.index('r'))
+# len method
+print(len(newtuple))
+# max method
+print(max(newtuple))
+print(min(newtuple))
+# tuple method
+print(tuple([1, 2, 3, 4]))
