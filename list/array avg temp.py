03f332566cbe5576101c38f23a522312f3564899
MERCY <obedmercy2018@gmail.com>
2022-08-04 01:57:39 +0100

    to  calculate average temperature of given days using list

---
+++ list/array avg temp.py
@@ -0,0 +1,15 @@
+# calculate average temperature of given days using list
+numdays = int(input('how many days temperature? '))
+total = 0
+temp = []
+for i in range(numdays):
+    nextday = int(input('Day' + str(i + 1) + ' high temperature ?'))
+    temp.append(nextday)
+    total += nextday
+average = round(total / numdays, 2)
+print('Average = ', average)
+above = 0
+for i in temp:
+    if i > average:
+        above += 1
+print(str(above) + " day(s) are above average")
