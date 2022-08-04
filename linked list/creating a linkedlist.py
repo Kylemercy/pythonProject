be5c4545ef9156f499b73a3d531994d9b421b141
MERCY <obedmercy2018@gmail.com>
2022-08-04 12:31:32 +0100

    creating a single linked list

--- linked list/creating a linkedlist.py
+++ linked list/creating a linkedlist.py
@@ -1,1 +1,15 @@
 # creation of singly linked list
+class Node:
+    def __init__(self,value = None):
+        self.value = value
+        self.next = None
+class slinkedlist:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+singlylinkedlist = slinkedlist()
+node1 = Node()
+node2 = Node()
+singlylinkedlist.head = node1
+singlylinkedlist.head.next = node2
+singlylinkedlist.tail = node2
