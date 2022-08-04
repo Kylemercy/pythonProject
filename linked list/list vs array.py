458664de345f092268fe89f88c9bcd89fd590f1e
MERCY <obedmercy2018@gmail.com>
2022-08-04 02:01:37 +0100

    to  calculate average temperature of given days using list

--- linked list/list vs array.py
+++
@@ -1,8 +0,0 @@
-#array vs list
-import numpy as np
-array1 = np.array([1,2,3,4,5])
-my_list = [6,7,8,9,10]
-print(my_list/2)
-#this gives and error but for array it performs the division
-print(array1/2)
-#for array the data type must be thesame
