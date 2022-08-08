0fb618e68237ec1eca5125c1c282f0e715e5024a
MERCY <obedmercy2018@gmail.com>
2022-08-08 13:22:02 +0100

    prints different name from diven names

--- learn python/banker roullete.py
+++ learn python/banker roullete.py
@@ -2,9 +2,7 @@ import random
 names_string = input("Give me everybody's names,separated by a comma.\n")
 names = names_string.split(",")
 n = len(names)
-txt = random.randint(0,n-1
-                     )
-#print(txt)
+txt = random.randint(0,n-1)
 person_to_pay = names[txt]
 print(person_to_pay)
 print(person_to_pay + ' is going to buy the meal today!')
