59353e88dc2221ddba5ad885dba845a6c1d6addb
MERCY <obedmercy2018@gmail.com>
2022-08-08 13:57:49 +0100

    adding 2 lists together

---
+++ learn python/dirty dozen.py
@@ -0,0 +1,4 @@
+fruits = ['pineapple', 'guava', 'orange', 'paw_paw', 'mango', 'kiwi', 'watermelon', 'apple']
+vegetables = ['cabbage', 'spinach', 'tomato', 'carrot', 'bell_pepper', 'potatoes']
+dity_dozen = [fruits,vegetables]
+print(dity_dozen)
