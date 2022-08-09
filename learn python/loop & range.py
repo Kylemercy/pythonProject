1f438f08aa55b401aba2a269df8075d74b815207
MERCY <obedmercy2018@gmail.com>
2022-08-09 12:36:28 +0100

    loop and range

---
+++ learn python/loop & range.py
@@ -0,0 +1,13 @@
+# using for loop with the range function
+for i in range(1,21):
+    print(i)
+
+for i in range(1,21,3):
+    print(i)
+# sum num in given range
+total = 0
+for i in range(1,101):
+    total += i
+print(total)
+
+
