56ac0a8c22649c0192d67257d10c9745c224dde5
MERCY <obedmercy2018@gmail.com>
2022-08-09 17:53:06 +0100

    frizz buzz game

--- learn python/fizz buzz.py
+++ learn python/fizz buzz.py
@@ -2,15 +2,15 @@
 for i in range(1, 51):
     if i % 5 == 0 and i % 3 == 0:
         print('FrizzBuzz')
-        continue
+
 
     elif i % 5 == 0:
         print('Frizz')
-        continue
+
 
     elif i % 3 == 0:
         print('Buzz')
-        continue
+
 
     else:
         print(i)
