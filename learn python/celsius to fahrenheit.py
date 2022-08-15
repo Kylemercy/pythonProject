6a61dd0e66c77467e7ccdf0e350283747d334356
MERCY <obedmercy2018@gmail.com>
2022-08-15 16:19:35 +0100

    converting a given temperature from celsius to fahrenheit

---
+++ learn python/celsius to fahrenheit.py
@@ -0,0 +1,4 @@
+print('converting temperature from celsius to fahrenheit')
+cel = float(input('enter your temperature in celsius: '))
+temperature = round(((cel * 9) / 5) + 32,2)
+print('Your temperature in fahrenheit is: ', temperature , 'fahrenheit')
