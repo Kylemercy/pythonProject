401c4e38e4d03e7ae83ddd6af9047d85d552f45a
MERCY <obedmercy2018@gmail.com>
2022-08-20 16:06:26 +0100

    sum of 2 pairs of number using hash map with time comp of O(N)

---
+++ array/2sum.py
@@ -0,0 +1,21 @@
+# using a hashmap
+def two_sum(num, target):
+    result = []
+    # list of stored result
+    hash_map = {}
+    # dictionary difference values then index
+    for i, n in enumerate(num):
+        # loop for esch element
+        difference = target - n
+        if difference in hash_map:
+            result.append(i)
+            # appends the two values
+            result.append(hash_map[difference])
+        else:
+            hash_map[n] = i
+            # adds the difference value and index to the dictionary
+    return result
+
+
+num = [1, 2, 3, 4, 5, ]
+print(two_sum(num, 9))
