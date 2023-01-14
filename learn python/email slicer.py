6224400e3e14624096f94047ccd713a9fe54ce42
MERCY <obedmercy2018@gmail.com>
2023-01-14 11:27:26 +0100

    email slicer into domain name and user name

--- learn python/email slicer.py
+++ learn python/email slicer.py
@@ -1,5 +1,5 @@
 # email slicer
 email = input("Enter your email: \n").strip()
-username = email[: email.index("@")] #this stops immediately it reaches the @
-domain = email[email.index("@")+1:] # this starts immmediately after @ 
+username = email[: email.index("@")]  # this stops immediately it reaches the @
+domain = email[email.index("@") + 1:]  # this starts immediately after @
 print(f"your username is {username}.\ndomain name is {domain}.")
