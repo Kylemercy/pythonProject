0b8e4a5080354a8239382d4e2fa70e9a1cd42396
MERCY <obedmercy2018@gmail.com>
2023-01-30 04:34:13 +0100

    class and logging

---
+++ oop(object oriented)/class&logging.py
@@ -0,0 +1,21 @@
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='employee.log', level=logging.INFO, format='%(levelname)s: %(name)s: %(message)s')
+
+
+class employee:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        logger.info(" Created employee fullname {self.fullname}-- email {self.email} ")
+
+    @property
+    def email(self):
+        return f'{self.first}{self.last}@gmail.com'
+
+    @property
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+
+name1 = employee('jinnny', 'han')
