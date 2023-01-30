90ddbbaf5df4cc711ca2f7d7fa9f1a29f7d2e897
MERCY <obedmercy2018@gmail.com>
2023-01-30 04:37:55 +0100

    working with logging

---
+++ oop(object oriented)/log2.py
@@ -0,0 +1,8 @@
+import logging
+#logging levels
+logging.debug('debug')
+logging.info('info')
+logging.warning('warning')
+logging.error('error')
+logging.critical('critical')
+#from the least important to the most imortant
